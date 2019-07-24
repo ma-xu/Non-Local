69c72cd7779cda780537618590af6c4d15c799fa
13952522076 <melody@dynamic-10-125-247-107.dynamic.unt.edu>
2019-07-24 15:27:15 -0500

    GCNet in Multiplication fashion

--- models/cifar/__init__.py
+++ models/cifar/__init__.py
@@ -3,3 +3,4 @@ from .resnet import *
 from .se_resnet import *
 from .non_local_resnet import *
 from .se_add_resnet import *
+from .gc_mul_resnet import *
---
+++ models/cifar/gc_mul_resnet.py
@@ -0,0 +1,321 @@
+"""
+GC_ResNet in Multiplication fashion.
+We add a Sigmoid function in the end of Transform module.
+
+Author:     Xu Ma.
+Date:       Jul/24/2019
+Email:      xuma@my.unt.edu
+"""
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+from torch.nn.parameter import Parameter
+import torch
+import torch.nn.functional as F
+from torch.nn import init
+from torch.autograd import Variable
+from collections import OrderedDict
+import math
+
+
+
+__all__ = ['gc_mul_resnet18', 'gc_mul_resnet34', 'gc_mul_resnet50', 'gc_mul_resnet101', 'gc_mul_resnet152']
+
+#################################################
+def constant_init(module, val, bias=0):
+    nn.init.constant_(module.weight, val)
+    if hasattr(module, 'bias') and module.bias is not None:
+        nn.init.constant_(module.bias, bias)
+
+def kaiming_init(module,
+                 a=0,
+                 mode='fan_out',
+                 nonlinearity='relu',
+                 bias=0,
+                 distribution='normal'):
+    assert distribution in ['uniform', 'normal']
+    if distribution == 'uniform':
+        nn.init.kaiming_uniform_(
+            module.weight, a=a, mode=mode, nonlinearity=nonlinearity)
+    else:
+        nn.init.kaiming_normal_(
+            module.weight, a=a, mode=mode, nonlinearity=nonlinearity)
+    if hasattr(module, 'bias') and module.bias is not None:
+        nn.init.constant_(module.bias, bias)
+
+def last_zero_init(m):
+    if isinstance(m, nn.Sequential):
+        constant_init(m[-1], val=0)
+        m[-1].inited = True
+    else:
+        constant_init(m, val=0)
+        m.inited = True
+
+
+class ContextMULBlock2d(nn.Module):
+
+    def __init__(self, channel, reduction = 16):
+        super(ContextMULBlock2d, self).__init__()
+        self.conv_mask = nn.Conv2d(channel, 1, kernel_size=1)
+        self.softmax = nn.Softmax(dim=2)
+        self.channel_mul_conv = nn.Sequential(
+            nn.Conv2d(channel, channel//reduction, kernel_size=1),
+            nn.LayerNorm([channel//reduction, 1, 1]),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel//reduction, channel, kernel_size=1)
+        )
+
+        # init
+        kaiming_init(self.conv_mask, mode='fan_in')
+        self.conv_mask.inited = True
+        last_zero_init(self.channel_mul_conv)
+
+    def context_modeling(self,x):
+        N, C, H, W = x.size()
+        g = x
+        g = g.view(N, C, H * W)  # [N,C,H*W]
+        phi = self.conv_mask(x)  # [N,1,H,W]
+        phi = phi.view(N, 1, H * W)  # [N,1,H*W]
+        phi = self.softmax(phi)  # [N,1,H*W]
+        phi = phi.permute(0, 2, 1)  # [N,H*W,1]
+        context = torch.matmul(g, phi)  # [N,C,1]
+        context = context.unsqueeze(-1)  # [N,C,1,1]
+        return context
+
+    def forward(self, x):
+        # [N, C, 1, 1]
+        context = self.context_modeling(x)
+        out = x
+        # [N, C, 1, 1]
+        channel_mul_term = self.channel_mul_conv(context)
+        channel_mul_term = torch.sigmoid(channel_mul_term)
+        out = out * channel_mul_term
+        return out
+#################################################
+
+
+
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+        self.gc_mul_Block = ContextMULBlock2d(channel=planes*self.expansion)
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.gc_mul_Block(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = conv1x1(inplanes, planes)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes, stride)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = conv1x1(planes, planes * self.expansion)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+        self.gc_mul_Block = ContextMULBlock2d(channel=planes * self.expansion)
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out = self.gc_mul_Block(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False):
+        super(ResNet, self).__init__()
+        self.inplanes = 64
+
+        # For ImageNet
+        # self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+        #                        bias=False)
+        # self.bn1 = nn.BatchNorm2d(64)
+        # self.relu = nn.ReLU(inplace=True)
+        # self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # For CIFAR
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        # For ImageNet
+        # x = self.conv1(x)
+        # x = self.bn1(x)
+        # x = self.relu(x)
+        # x = self.maxpool(x)
+
+        # For CIFAR
+        x = F.relu(self.bn1(self.conv1(x)))
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+def gc_mul_resnet18(pretrained=False, **kwargs):
+    """Constructs a ResNet-18 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    return model
+
+
+def gc_mul_resnet34(pretrained=False, **kwargs):
+    """Constructs a ResNet-34 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+    return model
+
+
+def gc_mul_resnet50(pretrained=False, **kwargs):
+    """Constructs a ResNet-50 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    return model
+
+
+def gc_mul_resnet101(pretrained=False, **kwargs):
+    """Constructs a ResNet-101 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+    return model
+
+
+def gc_mul_resnet152(pretrained=False, **kwargs):
+    """Constructs a ResNet-152 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+    return model
+
+
+def demo():
+    net = gc_mul_resnet101(num_classes=100)
+    y = net(torch.randn(2,3,32,32))
+    print(y.size())
+
+demo()
+
+def demoGC():
+    gcBlock = ContextMULBlock2d(channel=64)
+    print(gcBlock)
+    print(gcBlock(torch.randn(2,64,32,32)).size())
+
+
+demoGC()
